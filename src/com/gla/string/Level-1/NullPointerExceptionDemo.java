e57e3dd30d273359a309af0ae83f73b487d5bb1e
sonirishav56-lab <sonirishav56@gmail.com>
2026-03-16 21:03:43 +0530

    Added files

---
+++ src/com/gla/string/Level-1/NullPointerExceptionDemo.java
@@ -0,0 +1,18 @@
+﻿package com.gla.string;
+
+public class NullPointerExceptionDemo {
+    public static void main(String[] args) {
+        try {
+            int length = getNullStringLength();
+            System.out.println("Length: " + length);
+        } catch (NullPointerException e) {
+            System.out.println("Caught NullPointerException: " + e.getMessage());
+        }
+    }
+
+    private static int getNullStringLength() {
+        String text = null;
+        return text.length();
+    }
+}
+
