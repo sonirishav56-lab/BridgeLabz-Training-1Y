f90ea53510964853e6bb774ac4530da08f830167
sonirishav56-lab <sonirishav56@gmail.com>
2026-03-16 21:49:28 +0530

    Delete interfaces-abstraction directory

--- interfaces-abstraction/src/com/gla/interfacesabstraction/level1/Main.java
+++
@@ -1,7 +0,0 @@
-package com.gla.interfacesabstraction.level1;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Starter for interfacesabstraction level1");
-    }
-}
