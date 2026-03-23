0319e6969975e747db2c166a7ea918e44ea698d8
sonirishav56-lab <sonirishav56@gmail.com>
2026-03-23 14:23:48 +0530

    Delete inheritance-polymorphism/src/com/gla/inheritancepolymorphism directory

--- inheritance-polymorphism/src/com/gla/inheritancepolymorphism/hybridinheritance/Person.java
+++
@@ -1,9 +0,0 @@
-package com.gla.inheritancepolymorphism.hybridinheritance;
-
-public class Person {
-    protected String name;
-
-    public Person(String name) {
-        this.name = name;
-    }
-}
--- inheritance-polymorphism/src/com/gla/inheritancepolymorphism/hybridinheritance/Refuelable.java
+++
@@ -1,5 +0,0 @@
-package com.gla.inheritancepolymorphism.hybridinheritance;
-
-public interface Refuelable {
-    String refuel();
-}
