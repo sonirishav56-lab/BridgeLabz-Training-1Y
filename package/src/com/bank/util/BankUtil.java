b94c0d05da5571d7ef7ff1359224f53125a32892
sonirishav56-lab <sonirishav56@gmail.com>
2026-03-23 14:25:15 +0530

    Delete package/src directory

--- package/src/com/bank/util/BankUtil.java
+++
@@ -1,7 +0,0 @@
-package com.bank.util;
-
-public class BankUtil {
-    public boolean isValidAccountNumber(String accountNumber) {
-        return accountNumber != null && accountNumber.length() >= 8;
-    }
-}
--- package/src/com/school/util/DataFormatter.java
+++
@@ -1,7 +0,0 @@
-package com.school.util;
-
-public class DataFormatter {
-    public String formatRollNumber(int rollNumber) {
-        return "ROLL-" + rollNumber;
-    }
-}
