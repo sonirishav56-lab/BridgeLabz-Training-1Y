2de58f72b5851f6b17e510cca2011b7f7918b6fc
sonirishav56-lab <sonirishav56@gmail.com>
2026-03-31 14:38:19 +0530

    Delete package/src directory

--- package/src/com/access/one/AccessOneHelper.java
+++
@@ -1,7 +0,0 @@
-package com.access.one;
-
-public class AccessOneHelper {
-    public String getSourceName() {
-        return "AccessOne";
-    }
-}
--- package/src/com/company/payroll/PayrollCalculator.java
+++
@@ -1,7 +0,0 @@
-package com.company.payroll;
-
-public class PayrollCalculator {
-    public double calculateNetPay(double basicPay, double deductions) {
-        return basicPay - deductions;
-    }
-}
